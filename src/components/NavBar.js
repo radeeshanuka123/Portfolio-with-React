5d9639bc0becb92d9899def2a2b5372255c82841
radeeshanuka123 <87101113+radeeshanuka123@users.noreply.github.com>
2024-09-18 10:27:03 +0530

    Logo updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <h2 className="logo"> Shanuka Radeeshan </h2>   
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
